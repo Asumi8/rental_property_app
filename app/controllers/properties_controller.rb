4189f32e260bc060b9b9784dbf903f4b43d30a93
asumi nakajima <nakajima0848@gmail.com>
2022-10-20 15:43:54 +0900

    ビューの作成、コントローラの修正

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,69 +1,74 @@
 class PropertiesController < ApplicationController
-  before_action :set_property, only: %i[ show edit update destroy ]
 
-  # GET /properties or /properties.json
   def index
     @properties = Property.all
   end
 
-  # GET /properties/1 or /properties/1.json
-  def show
-  end
-
-  # GET /properties/new
   def new
     @property = Property.new
+    2.times{@property.nearest_stations.build}
   end
 
-  # GET /properties/1/edit
-  def edit
-  end
-
-  # POST /properties or /properties.json
   def create
     @property = Property.new(property_params)
-
-    respond_to do |format|
-      if @property.save
-        format.html { redirect_to @property, notice: "Property was successfully created." }
-        format.json { render :show, status: :created, location: @property }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @property.errors, status: :unprocessable_entity }
-      end
+    if @property.save
+      redirect_to properties_path
+    else  
+      render :new
     end
+    # respond_to do |format|
+    #   if @property.save
+    #     format.html { redirect_to @property, notice: "Property was successfully created." }
+    #     format.json { render :show, status: :created, location: @property }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @property.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
-  # PATCH/PUT /properties/1 or /properties/1.json
+  def show
+    @property = Property.find(params[:id])
+  end
+
+  def edit
+    @property = Property.find(params[:id])
+    @property.nearest_stations.build
+  end
+
   def update
-    respond_to do |format|
-      if @property.update(property_params)
-        format.html { redirect_to @property, notice: "Property was successfully updated." }
-        format.json { render :show, status: :ok, location: @property }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @property.errors, status: :unprocessable_entity }
-      end
+    @property = Property.find(params[:id])
+    if @property.update(property_params)
+      redirect_to properties_path 
+    else 
+      render :edit
     end
+    # respond_to do |format|
+    #   if @property.update(property_params)
+    #     format.html { redirect_to @property, notice: "Property was successfully updated." }
+    #     format.json { render :show, status: :ok, location: @property }
+    #   else
+    #     format.html { render :edit, status: :unprocessable_entity }
+    #     format.json { render json: @property.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /properties/1 or /properties/1.json
   def destroy
+    @property = Property.find(params[:id])
     @property.destroy
-    respond_to do |format|
-      format.html { redirect_to properties_url, notice: "Property was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to properties_path
+    # respond_to do |format|
+    #   format.html { redirect_to properties_url, notice: "Property was successfully destroyed." }
+    #   format.json { head :no_content }
+    # end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_property
-      @property = Property.find(params[:id])
+
+    def property_params
+      params.require(:property).permit(:property_name, :rent, :address, :building_age, :remark, nearest_stations_attributes:[:id, :line, :station_name, :walking_minutes])
     end
 
-    # Only allow a list of trusted parameters through.
-    def property_params
-      params.require(:property).permit(:property_name, :rent, :address, :building_age, :remark)
-    end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,7 @@
 class Property < ApplicationRecord
-  has_many :nearest_stations
-  rails_accepts_nested_attributes_for :nearest_stations, reject_if: proc {|attributes| attributes['line'].blank? and attributes['station_name'].blank? and attributes['walking_minutes'].blank?}, allow_destroy:true
+  validates :property_name, :rent, :address, :building_age, presence: true 
+  has_many :nearest_stations, dependent: :destroy
+  accepts_nested_attributes_for :nearest_stations, reject_if: proc {|attributes| attributes['line'].blank? and attributes['station_name'].blank? and attributes['walking_minutes'].blank?}, allow_destroy:true
 end
+
+
