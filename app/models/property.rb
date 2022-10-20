d3e3fa67651205de489a344be14b04efadca5103
asumi nakajima <nakajima0848@gmail.com>
2022-10-20 09:33:42 +0900

    モデルの作成、設定

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,4 @@
 class Property < ApplicationRecord
+  has_many :nearest_stations
+  rails_accepts_nested_attributes_for :nearest_stations, reject_if: proc {|attributes| attributes['line'].blank? and attributes['station_name'].blank? and attributes['walking_minutes'].blank?}, allow_destroy:true
 end
